c84b8417d95d756387a3bb39f6f9a8ef0c0b8f10
Dylan <dylanc176@nycstudents.net>
2023-09-21 17:12:51 -0400

    added freestyle (tax)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -15,6 +15,10 @@ public class TipCalculator {
         int tipPercentage = scan.nextInt();
         scan.nextLine();
 
+        System.out.print("Enter tax percentage (do not include % symbol): ");
+        int taxPercentage = scan.nextInt();
+        scan.nextLine();
+
         while (true) {
             System.out.print("Enter a cost in dollars and cents (-1 to end): ");
             cost = scan.nextDouble();
@@ -23,19 +27,31 @@ public class TipCalculator {
         }
         double totalTip = (totalBill * (tipPercentage / 100.0));
         totalTip = Math.round(totalTip * 100.0) / 100.0;
-        double totalBillWithTip = (totalBill + totalTip);
+
+        double totalTax = (totalBill * (taxPercentage / 100.0));
+        totalTax = Math.round(totalTax * 100.0) / 100.0;
+
+        double totalAmount = (totalBill + totalTip + totalTax);
+
         double costPerPerson = (totalBill / numParty);
         costPerPerson = Math.round(costPerPerson * 100.0) / 100.0;
+
         double tipPerPerson = (totalTip / numParty);
         tipPerPerson = Math.round(tipPerPerson * 100.0) / 100.0;
+
+        double taxPerPerson = (totalTax / numParty);
+        taxPerPerson = Math.round(taxPerPerson * 100.0) / 100.0;
+
         double totalCostPerPerson = (costPerPerson + tipPerPerson);
 
         System.out.println("Total bill before tip: $" + totalBill);
         System.out.println("Total percentage: %" + tipPercentage);
         System.out.println("Total tip: $" + totalTip);
-        System.out.println("Total bill with tip: $" + totalBillWithTip);
-        System.out.println("Per person cost before tip: $" + costPerPerson);
+        System.out.println("Total tax: $" + totalTax);
+        System.out.println("Total bill with tip and tax: $" + totalAmount);
+        System.out.println("Per person cost before tip and tax: $" + costPerPerson);
         System.out.println("Tip per person: $" + tipPerPerson);
+        System.out.println("Tax per person: $" + taxPerPerson);
         System.out.println("Total cost per person: $" + totalCostPerPerson);
 
     }
